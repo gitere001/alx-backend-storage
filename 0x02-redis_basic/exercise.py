72037a2668f1ff5dbded52e236d3aac2d2176728
James gitere <sasmuypwa2019.com>
2024-07-17 12:03:44 +0300

    updated exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """contains the cache class"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -34,3 +34,52 @@ class Cache:
 
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]=None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieves the value associated with the given key from the Redis cache.
+
+        Parameters:
+            key (str): The key used to identify the value in the cache.
+            fn (Optional[Callable]): An optional function to apply to the
+            retrieved value before returning it. Defaults to None.
+
+        Returns:
+            The value associated with the given key, or None if the key does
+            not exist in the cache.
+            If a function is provided, the value is passed to the function
+            before being returned.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieves the value associated with the given key from the Redis cache
+        and decodes it as a UTF-8 string.
+
+        Parameters:
+            key (str): The key used to identify the value in the cache.
+
+        Returns:
+            str: The decoded value associated with the given key, or None if
+            the key does not exist in the cache.
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieves the value associated with the given key from the Redis cache
+        and converts it to an integer.
+
+        Parameters:
+            key (str): The key used to identify the value in the cache.
+
+        Returns:
+            int: The integer value associated with the given key, or None if
+            the key does not exist in the cache.
+
+        """
+        return self.get(key, int)
